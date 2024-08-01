8e33f7f615a05f1fe701264abad281482128a973
Sameer Kharel <110817285+Kharelsameer@users.noreply.github.com>
2024-08-01 20:13:13 +1000

    image search

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,30 @@
-const ACCESS_KEY = "";
+const ACCESS_KEY = "SonkqFe1zfl2gZFKkoNvkB_83GgTDVrMKLm0fT7ZYTo";
 const SECRET_KEY = "";
 
+const labels = ['Montreal', 'Canada', 'Collective', 'Table', 'Interior', 'Chair', 'Coffee Shop', 'Coffee Machine', 'Coffee', 'Cafe'];
+function populateLabels(){
+    const labelElements = labels.map(label => {
+        return `<button class="label-btn">${label}</button>`;
+    });
+    document.querySelector('.labels-wrap').innerHTML = labelElements.join("");
+
+    document.querySelectorAll('.label-btn').forEach(button => {
+        button.addEventListener('click', async (event) => {
+            const query = event.target.textContent;
+            await searchImages(query);
+        })
+    })
+}
+populateLabels();
 
 document.querySelector('form').addEventListener('submit', async function(event) {
     event.preventDefault();
-
     const query = document.querySelector("#search").value;
+    await searchImages(query);
+    
+})
+async function searchImages(query){
+
     console.log({query});
     const url = `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${ACCESS_KEY}`;
     console.log({url})
@@ -21,4 +40,4 @@ document.querySelector('form').addEventListener('submit', async function(event) 
         console.log({imageUrl});
     }
     document.querySelector('.images').innerHTML = imageElements;
-})
+}
